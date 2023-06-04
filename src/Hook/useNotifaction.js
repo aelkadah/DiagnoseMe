8c1610c2b1efc7af8508590d4c3ceddeddb5c4b0
Ahmed ElKadah <ahmedaymanelkadah@gmail.com>
2023-06-04 19:21:13 +0300

    done

--- src/Hook/useNotifaction.js
+++ src/Hook/useNotifaction.js
@@ -1,15 +1,11 @@
-
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 //to make notifactio to any componentet
 const notify = (msg, type) => {
-    if (type === "warn")
-        toast.warn(msg)
-    else if (type ==="success")
-        toast.success(msg)
-    else if (type ==="error")
-        toast.error(msg)
+  if (type === "warn") toast.warn(msg);
+  else if (type === "success") toast.success(msg);
+  else if (type === "error") toast.error(msg);
 };
 
 export default notify;
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,11 +1,24 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import SecTitle from "../components/utilities/SecTitle";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalendarAlt,
+  faHeartCircleBolt,
+  faHeartCircleCheck,
+} from "@fortawesome/free-solid-svg-icons";
 
 const DashboardPage = () => {
   return (
     <Container>
-      <SecTitle title="Dashboard" />
+      {/* <SecTitle title="Dashboard" /> */}
+      <Row className="d-flex justify-content-between align-items-center">
+        <h5 className="w-auto">Dashboard</h5>
+        <h6 className="w-auto d-flex align-items-center">
+          <FontAwesomeIcon icon={faCalendarAlt} className="ms-0 me-1" />
+          04/06/2023
+        </h6>
+      </Row>
     </Container>
   );
 };
