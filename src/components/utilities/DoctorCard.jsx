6d0e0226840933558fd013412137cfe159851e32
Ahmed ElKadah <ahmedaymanelkadah@gmail.com>
2023-06-05 12:34:43 +0300

    doctorCard Link with id

--- src/components/utilities/DoctorCard.jsx
+++ src/components/utilities/DoctorCard.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const DoctorCard = ({ doctor }) => {
   return (
     // <Col sm={12} md={6} lg={6}>
-    <Link to={`/doctors/:id`}>
+    <Link to={`/doctors/${doctor?.id}`}>
       <div className="card mb-3 card-doctors" style={{ maxwidth: "540px" }}>
         <div className="row g-0 align-items-center">
           <div className="col-md-4">
@@ -67,18 +67,6 @@ const DoctorCard = ({ doctor }) => {
                   {doctor.PhoneNumber}
                 </small>
               </p>
-              <a href="#">
-                <span
-                  style={{
-                    color: "black",
-                    fontWeight: "bold",
-                    marginRight: "10px",
-                  }}
-                >
-                  id:
-                </span>
-                {doctor.id}
-              </a>
             </div>
           </div>
         </div>
