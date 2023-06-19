7879a55a8e315490d376e66c3c30f83c51b2cae2
Ahmed ElKadah <ahmedaymanelkadah@gmail.com>
2023-06-20 01:24:28 +0300

    Condition Card done

--- src/components/Conditions/ConditionCard.jsx
+++ src/components/Conditions/ConditionCard.jsx
@@ -1,37 +1,45 @@
 import React from "react";
-import { Row, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { Card } from "react-bootstrap";
 
 const ConditionCard = ({ condition }) => {
-  const symptoms = condition?.symptoms?.split(",");
+  // const symptoms = condition?.symptoms?.split(",");
 
   return (
     <Link to={`/conditions/${condition?.id}`}>
-      <Row className="mt-5">
-        <Card className="newsCard">
-          <Card.Body>
-            <Card.Text className="fs-4 fw-bold mt-1 mb-2">
-              {condition?.overview}
-            </Card.Text>
-            <Card.Text className="fw-normal">
-              <h6 className="w-auto text-secondary">
-                Symptoms:
-                <ul className="text-black fw-normal ">
-                  {symptoms
-                    ?.map((sym, index) => (
-                      <li className="truncateOne" key={index}>
-                        {sym}
-                      </li>
-                    ))
-                    .slice(0, 3)}
-                </ul>
-              </h6>
-            </Card.Text>
-            <p className="text-primary fw-normal mt-2 mb-1">Read more</p>
-          </Card.Body>
-        </Card>
-      </Row>
+      <Card className="bg-light border-0 text-black mb-4">
+        <h4 className="fw-bold">{condition?.overview}</h4>
+        <p className="fw-normal truncateTwo">{condition?.symptoms}</p>
+        <p className="text-primary fw-normal mt-1 mb-2">Read more</p>
+      </Card>
     </Link>
+
+    // <Link to={`/conditions/${condition?.id}`}>
+    //   <Row className="mt-5">
+    //     <Card className="newsCard">
+    //       <Card.Body>
+    //         <Card.Text className="fs-4 fw-bold mt-1 mb-2">
+    //           {condition?.overview}
+    //         </Card.Text>
+    //         <Card.Text className="fw-normal">
+    //           <h6 className="w-auto text-secondary">
+    //             Symptoms:
+    //             <ul className="text-black fw-normal ">
+    //               {symptoms
+    //                 ?.map((sym, index) => (
+    //                   <li className="truncateOne" key={index}>
+    //                     {sym}
+    //                   </li>
+    //                 ))
+    //                 .slice(0, 3)}
+    //             </ul>
+    //           </h6>
+    //         </Card.Text>
+    //         <p className="text-primary fw-normal mt-2 mb-1">Read more</p>
+    //       </Card.Body>
+    //     </Card>
+    //   </Row>
+    // </Link>
   );
 };
 
--- src/components/Conditions/ConditionsContainer.jsx
+++ src/components/Conditions/ConditionsContainer.jsx
@@ -3,6 +3,7 @@ import { Container, Row, Col, Spinner } from "react-bootstrap";
 import ConditionCard from "./ConditionCard";
 import ConditionsHook from "../../redux/Hooks/Conditions/ConditionsHook";
 import { Pagination } from "../../components";
+import { Link } from "react-router-dom";
 
 const ConditionsContainer = () => {
   const [conditions, loading, pageCount, getPage] = ConditionsHook();
@@ -13,9 +14,18 @@ const ConditionsContainer = () => {
         conditions?.data?.length >= 1 ? (
           conditions?.data.map((condition, index) => {
             return (
-              <Col xs={12} md={4} key={index}>
+              <Col xs={12} md={6} lg={4} key={index}>
                 <ConditionCard condition={condition} />
               </Col>
+              // <Col xs={12} md={6} lg={4} key={index}>
+              //   <Row className="bg-light px-3 py-4 rounded rounded-3">
+              //     <h3 className="fw-bold">Blooood</h3>
+              //     <h6>asudbcan sckasjbcas cxjh</h6>
+              //     <Link to={`/conditions/:id`} className="mt-2">
+              //       Read more
+              //     </Link>
+              //   </Row>
+              // </Col>
             );
           })
         ) : (
