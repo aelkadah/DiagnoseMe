502686b2709500fcf8632f2da70aad9d11236631
Ahmed ElKadah <ahmedaymanelkadah@gmail.com>
2023-06-20 13:41:38 +0300

    Section Header py-0

--- src/components/utilities/SecTitle.jsx
+++ src/components/utilities/SecTitle.jsx
@@ -4,7 +4,7 @@ import { Row } from "react-bootstrap";
 const SecTitle = ({ title, desc }) => {
   if (title && title != "")
     return (
-      <Row className="py-4">
+      <Row>
         <h1 className="secTitle">{title}</h1>
         {desc && desc != "" ? <p>{desc}</p> : null}
       </Row>
